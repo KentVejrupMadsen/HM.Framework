fbe54477a83e98744ab9dc0c7a49372e79959bc8
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-05-28 00:15:27 +0200

    organising code
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- code/globals.py
+++ code/globals.py
@@ -1,11 +1,12 @@
 path_to_project_folder: None | str = None
+path_to_library_folder: None | str = None
+
+path_to_temporary_folder: None | str = None
 path_to_output_folder: None | str = None
 
-path_to_library_folder: None | str = None
-path_to_temporary_folder: None | str = None
 
-
-# Getters
+# Accessors
+# * Getters
 def get_path_to_project_folder() -> None | str:
     global path_to_project_folder
     return path_to_project_folder
@@ -26,7 +27,7 @@ def get_path_to_temporary_folder() -> None | str:
     return path_to_temporary_folder
 
 
-# Setters
+# * Setters
 def set_path_to_project_folder(
         value: str
 ) -> None:
@@ -34,17 +35,17 @@ def set_path_to_project_folder(
     path_to_project_folder = value
 
 
-def set_path_to_output_folder( value: str ) -> None:
+def set_path_to_output_folder(value: str) -> None:
     global path_to_output_folder
     path_to_output_folder = value
 
 
-def set_path_to_library_folder( value: str ) -> None:
+def set_path_to_library_folder(value: str) -> None:
     global path_to_library_folder
     path_to_library_folder = value
 
 
-def set_path_to_temporary_folder( value: str ) -> None:
+def set_path_to_temporary_folder(value: str) -> None:
     global path_to_temporary_folder
     path_to_temporary_folder = value
 
