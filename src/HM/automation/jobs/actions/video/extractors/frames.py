1c184e94d54a4015fdcca578dbaab7f039054106
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-05-31 19:34:27 +0200

    restructuring
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- src/HM/automation/jobs/actions/video/extractors/frames.py
+++ src/HM/automation/jobs/actions/video/extractors/frames.py
@@ -8,9 +8,6 @@ from math \
     import \
     floor
 
-from moviepy.editor \
-    import VideoFileClip
-
 from HM.objects.operators.counter \
     import Counter
 
@@ -27,78 +24,31 @@ class Extractor:
         self.save_location: str = save_location
         self.path_to_video: str = path_to_video
 
-        self.video_clip: None | VideoFileClip = None
+        self.video_clip: None
         self.save_frames_as_format: str = save_frames_as
 
         self.debug_mode: bool = True
         self.snapshot_at: float = 1.0
 
     def __del__(self):
-        if not (self.video_clip is None):
-            self.video_clip.close()
-
-    def debug_show_location_message(
-            self,
-            message: str,
-            location: str
-    ):
-        if self.debug_mode:
-            print(
-                message,
-                ':',
-                location
-            )
+        pass
 
     def execute(self):
         if self.exist_video_file() \
            and \
            self.exist_save_location():
-
-            self.debug_show_location_message(
-                'found video',
-                self.get_path_to_video()
-            )
-
-            self.debug_show_location_message(
-                'found save location',
-                self.get_save_location()
-            )
+            pass
         else:
             return
 
         self.set_video_clip(
-            VideoFileClip(
-                self.get_path_to_video()
-            )
+            None
         )
 
         self.process_video()
 
     def process_video(self):
         counter_of_frames = self.get_counter()
-
-        duration_of_video = floor(
-            self.get_video_clip().duration
-        )
-
-        for frame_at in \
-                range(
-                    1,
-                    duration_of_video
-                ):
-
-            self.get_video_clip().save_frame(
-                str(
-                    join(
-                        self.get_save_location(),
-
-                        counter_of_frames.get_value_as_string() + '.' + self.get_save_frames_as_format()
-                    )
-                ),
-                t=frame_at
-            )
-
-            counter_of_frames.increment()
 
     def exist_video_file(self) -> bool:
         return isfile(
@@ -127,7 +77,7 @@ class Extractor:
 
     def get_video_clip(
             self
-    ) -> None | VideoFileClip:
+    ) -> None:
         return self.video_clip
 
     def get_save_frames_as_format(
@@ -161,7 +111,6 @@ class Extractor:
 
     def set_video_clip(
             self,
-            value: VideoFileClip | None
+            value: None
     ) -> None:
         self.video_clip = value
-
