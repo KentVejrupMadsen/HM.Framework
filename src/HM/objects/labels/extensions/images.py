974201799a4b41e706df7cd939c8b136505925f7
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-05-31 19:07:10 +0200

    branching

--- src/HM/objects/labels/extensions/images.py
+++ src/HM/objects/labels/extensions/images.py
@@ -13,5 +13,19 @@ def get_png_label() -> str:
 def get_tga_label() -> str:
     return 'tga'
 
+
 def get_bmp_label() -> str:
     return 'bmp'
+
+
+def get_dng_label() -> str:
+    return 'dng'
+
+
+def get_psd_label() -> str:
+    return 'psd'
+
+
+def get_psb_label() -> str:
+    return 'psb'
+
